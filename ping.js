b1cde058b18a57a3109571a8de96c13ce987ad6e
7468696e6b <61638924+7468696e6b@users.noreply.github.com>
2021-07-25 22:57:00 -0400

    Create ping.js
    sample code using discordjs/discord.js module

---
+++ ping.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * A ping pong bot, whenever you send "ping", it replies "pong".
+ * https://discord.js.org/#/docs/main/stable/examples/ping
+ * https://discordjs.guide/preparations/setting-up-a-bot-application.html#keeping-your-token-safe
+ */
+
+// Import the discord.js module
+const Discord = require('discord.js');
+// dotenv module to hide TOKEN from GitHub using .env and .gitignore
+const dotenv = require('dotenv');
+
+// Create an instance of a Discord client
+const client = new Discord.Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === 'ping') {
+    // Send "pong" to the same channel
+    message.channel.send('pong');
+  }
+});
+
+// Log our bot in using the token from https://discord.com/developers/applications
+// client.login('your token here');
+client.login(process.env.TOKEN);
