6bea6681dee6fd7b672389fe936df67699968072
7468696e6b <61638924+7468696e6b@users.noreply.github.com>
2021-11-15 23:42:08 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+// import our requisite libraries
+const Discord = require('discord.js');
+const dotenv = require('dotenv');
+
+// setup environment variables containing token api key, etc
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+
